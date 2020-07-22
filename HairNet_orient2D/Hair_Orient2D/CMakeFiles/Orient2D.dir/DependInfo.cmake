5674ed44529a723659499d989a2a88fb10a3108a
compass0 <bluesalt1621@gmail.com>
2020-07-22 16:58:01 +0000

    [orientation] add Hairnet_orient2D

---
+++ HairNet_orient2D/Hair_Orient2D/CMakeFiles/Orient2D.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/elastin/louis/HairNet_orient2D/Hair_Orient2D/main.cpp" "/data/elastin/louis/HairNet_orient2D/Hair_Orient2D/CMakeFiles/Orient2D.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/root/OpenMesh-6.3/src"
+  "/root/opencv/opencv/build"
+  "/root/opencv/opencv/include"
+  "/root/opencv/opencv/modules/core/include"
+  "/root/opencv/opencv/modules/flann/include"
+  "/root/opencv/opencv/modules/imgproc/include"
+  "/root/opencv/opencv/modules/ml/include"
+  "/root/opencv/opencv/modules/photo/include"
+  "/root/opencv/opencv/modules/dnn/include"
+  "/root/opencv/opencv/modules/features2d/include"
+  "/root/opencv/opencv/modules/imgcodecs/include"
+  "/root/opencv/opencv/modules/videoio/include"
+  "/root/opencv/opencv/modules/calib3d/include"
+  "/root/opencv/opencv/modules/highgui/include"
+  "/root/opencv/opencv/modules/objdetect/include"
+  "/root/opencv/opencv/modules/stitching/include"
+  "/root/opencv/opencv/modules/ts/include"
+  "/root/opencv/opencv/modules/video/include"
+  "/root/opencv/opencv/modules/gapi/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ HairNet_orient2D/Hair_Orient2D/CMakeFiles/Orient2D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Orient2D.dir/main.cpp.o"
+  "Orient2D"
+  "Orient2D.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Orient2D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
